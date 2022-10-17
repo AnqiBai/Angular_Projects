7d577ce3ab9fb8717459f58359110d201905f5ef
Anqi Bai <baianqi086@gmail.com>
2022-10-17 16:42:56 -0400

    Highlighted the query keyword.

--- cocktails/src/app/app.component.ts
+++ cocktails/src/app/app.component.ts
@@ -10,8 +10,11 @@ export class AppComponent {
   constructor(private cocktail: CocktailService) { }
 
   drinks = [];
+  query = '';
   search(query: string) {
     // console.log('app: ', query);
+    this.query = query;
+
     this.cocktail.search(query).subscribe((response: any) => {
       this.drinks = response.drinks;
     });
--- cocktails/src/app/results/results.component.ts
+++ cocktails/src/app/results/results.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
   @Input() drinks = [];
+  @Input() query = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  boldStr(str: string) {
+    if (this.query.length < 3) {
+      return str;
+    }
 
+    const reg = new RegExp('(' + this.query + ')', 'gi');
+    return str.replace(reg, '<span class="font-weight-bold">$1</span>');
+  }
 }

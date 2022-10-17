64deece2fb34d71e971ffce2aaf206e7052c82e7
Anqi Bai <baianqi086@gmail.com>
2022-10-17 16:57:48 -0400

    Added interface and generics.

--- cocktails/src/app/cocktail.service.ts
+++ cocktails/src/app/cocktail.service.ts
@@ -2,6 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { pluck } from 'rxjs/operators';
 
+interface CocktailResponse {
+  drinks: {
+    strDrink: string;
+    strInstructions: string;
+  }[]
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +17,7 @@ export class CocktailService {
   constructor(private http: HttpClient) { }
 
   public search(query: string) {
-    return this.http.get('https://www.thecocktaildb.com/api/json/v1/1/search.php', {
+    return this.http.get<CocktailResponse>('https://www.thecocktaildb.com/api/json/v1/1/search.php', {
       params: {
         s: query,
       }

9da81a3522b36ca23c932d55d97f2446fce45d0a
Anqi Bai <baianqi086@gmail.com>
2022-10-18 15:38:58 -0400

    Updated to a Input component.

--- credit-card/src/app/app.module.ts
+++ credit-card/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,

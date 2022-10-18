a70e5905820f067de5afd37f44eae330344a07f2
Anqi Bai <baianqi086@gmail.com>
2022-10-18 16:17:37 -0400

    Updated the Input component for validating other fields.

--- credit-card/src/app/app.component.ts
+++ credit-card/src/app/app.component.ts
@@ -11,6 +11,24 @@ export class AppComponent {
     name: new FormControl('', [
       Validators.required,
       Validators.minLength(3)
+    ]),
+    num: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16)
+    ]),
+    expiration: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+    ]),
+    cvv: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3)
     ])
-  })
+  });
+
+  submit() {
+    console.log('form submitted');
+  }
 }
